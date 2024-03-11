408a60efc9b1a125a1059b7984d4a34e9a2fd108
João Pedro Monteiro <154384458+monteiroojp@users.noreply.github.com>
2024-03-11 11:10:54 -0300

    random number

---
+++ exercícios/ex017/math.js
@@ -0,0 +1,9 @@
+
+x = 2.499999
+
+//Math.trunc(x)
+//Math.round()
+//Math.ceil()
+let z = Math.abs(x)
+console.log(z)
+
---
+++ exercícios/ex017/randomNumber.js
@@ -0,0 +1,13 @@
+
+let max = 150
+let min = 50
+
+
+z = Math.trunc(Math.random() * (max - min)) + min  
+
+console.log(z)
+
+
+const stringe = 'hello world'
+
+console.log(stringe[5])
