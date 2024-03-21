dcdd46f2867701c714032059d5fa5682a4f4a476
João Pedro Monteiro <154384458+monteiroojp@users.noreply.github.com>
2024-03-20 23:26:12 -0300

    end

--- exercícios/ex026/fetch.js
+++ exercícios/ex026/fetch.js
@@ -1,16 +1,38 @@
-fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-.then(response => {
+let nameInput = document.getElementById('input')
+const button = document.getElementById('button')
+const pokemonImg = document.getElementById('pokemonImg')
+let element = document.getElementById('element')
 
-    if(!response.ok){
-        throw new Error("Could not execute the fetch function taligado")
+async function fetchData(pokemonName) {
+
+    try{
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        console.log(response)
+        
+
+        if(!response.ok){
+            window.alert('Esse pokemon não está mais diponível no pokedex!')
+            throw new Error("Fetch API couldn't acess the data")
+        }
+
+        else{
+            const data = await response.json()
+            console.log(data)
+            const pokemonImgSrc = data.sprites.front_default 
+            pokemonImg.src = pokemonImgSrc
+            pokemonImg.style.display = 'block'
+
+        }
     }
 
-    else{
-        return response.json()
+    catch(error){
+        console.error(error)
     }
+}
+
+
+button.addEventListener('click', () =>{
+    let pokemonName = String(nameInput.value).toLowerCase().trim()
+    fetchData(pokemonName)
 })
-.then(data => console.log(data))
-.catch(error => console.error(error))
-
-
 
